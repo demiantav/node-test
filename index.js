7799979e9825d11204b0d24737b30547680e2d26
Demian Tavolaro <demiantavolaro6@gmail.com>
2024-02-14 17:30:27 -0300

    Modificaciones en rutas para utilizar Mongo

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import cors from "cors"
 import Note from "./models/note.js"
 
 
-
-
 const app = express()
+
+
+//Middlewares
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -18,12 +23,7 @@ const requestLogger = (request, response, next) => {
   
 }
 
-
-
-app.use(express.json())
 app.use(requestLogger)
-app.use(cors())
-app.use(express.static('dist'))
 
 let notes = [
     {
@@ -50,7 +50,6 @@ let notes = [
 
 
   
-
   
 
 
@@ -67,28 +66,61 @@ let notes = [
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+  app.get('/api/notes/:id', (request, response, next) => {
+    
+    const id =request.params.id
+
+    Note.findById(id).then(note =>{
+
+      if(note){
+
+        response.json(note)
+      
+      } else {
+
+        response.status(404).end();
+      }
+      
+    })
+    .catch(error => next(error))
+
+    //const note = notes.find(note => note.id === id)
 
 
-    if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  //   if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.status(404).end()
+  // }
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+  app.delete('/api/notes/:id', (request, response, error) => {
+    const id = request.params.id
+
+    Note.findByIdAndDelete(id)
+     .then(res => {
+      response.status(204).end()
+     })
+     .catch(error => next(error))
+   
   })
 
-  app.put('/api/notes/:id', (request, response) => {
+  app.put('/api/notes/:id', (request, response, next) => {
 
-    const id = Number(request.params.id);
+    const body = request.body;
+
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+
+
 
   })
 
@@ -111,11 +143,6 @@ let notes = [
         important: body.important || false,
       })
 
-      // notes = notes.concat(note)
-  
-      // response.json(note)
-      // console.log(note)
-
       note.save().then(savedNote => {
         response.json(savedNote);
         
@@ -125,22 +152,31 @@ let notes = [
     
   })
 
-  const generateID = () => {
-
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id)) 
-      : 0
-
-    return maxId + 1;
-
-
-
+  //Mediante este middleware toda peticion que llegue y no coincida con alguna de las rutas termina en este middle
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 
   }
+  
+  app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  
+  app.use(errorHandler)
 
   
+  
+  const PORT = process.env.PORT
+
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   })
